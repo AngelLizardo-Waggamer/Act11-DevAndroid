cd9e7a13b40e757871451bfe5dd205636e4efbbc
Ángel Lizardo <aahlizardo.britanico@gmail.com>
2025-11-24 01:08:12 -0600

    Creación de fragments y navigation
    Tengo sueño.
    
    Signed-off-by: Ángel Lizardo <aahlizardo.britanico@gmail.com>

---
+++ app/src/main/java/aahl/appbd/ui/manage/products/audit/ProductAuditing.java
@@ -0,0 +1,25 @@
+package aahl.appbd.ui.manage.products.audit;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import aahl.appbd.R;
+
+public class ProductAuditing extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_audit_products, container, false);
+
+        return view;
+    }
+
+}
---
+++ app/src/main/java/aahl/appbd/ui/manage/products/modification/ProductModify.java
@@ -0,0 +1,25 @@
+package aahl.appbd.ui.manage.products.modification;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import aahl.appbd.R;
+
+public class ProductModify extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.products_detailed, container, false);
+
+        return view;
+    }
+
+}
