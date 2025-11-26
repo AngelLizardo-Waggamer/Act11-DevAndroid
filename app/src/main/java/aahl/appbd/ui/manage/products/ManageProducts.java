0556d4812a661b5f21ac0c7c144223ac3412c8d2
Ángel Lizardo <aahlizardo.britanico@gmail.com>
2025-11-26 10:42:25 -0600

    Implementación de Hub de acciones de productos
    En este caso se implementó la lógica para la pantalla previa a la modificación y auditoría de productos. Faltan pruebas.
    
    Signed-off-by: Ángel Lizardo <aahlizardo.britanico@gmail.com>

--- app/src/main/java/aahl/appbd/ui/manage/products/ManageProducts.java
+++ app/src/main/java/aahl/appbd/ui/manage/products/ManageProducts.java
@@ -4,14 +4,39 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import aahl.appbd.R;
+import aahl.appbd.data.DBOps;
+import aahl.appbd.models.Inventory;
+import aahl.appbd.models.Product;
 
 public class ManageProducts extends Fragment {
+
+    private Spinner spinnerInventarios;
+    private List<Inventory> inventories = new ArrayList<>();
+    private Inventory selectedInventory;
+    private CardView cardModifyProducts, cardAuditProducts;
+    private boolean isAnyInventoryAvailable = false;
+
+    private DBOps dbOps;
 
     @Nullable
     @Override
@@ -19,7 +44,125 @@ public class ManageProducts extends Fragment {
 
         View view = inflater.inflate(R.layout.hub_products, container, false);
 
+        // Inicializar el contexto de la base de datos
+        dbOps = new DBOps(getContext());
+
+        // Inicializar componentes
+        spinnerInventarios = view.findViewById(R.id.spinnerHubInventory);
+        cardModifyProducts = view.findViewById(R.id.cardModifyProducts);
+        cardAuditProducts = view.findViewById(R.id.cardAuditProducts);
+
+        // Configurar el spinner
+        // A como en el visualize, de una correcta inicialización depende
+        // el resto del código
+        configurarSpinner(view);
+
+        // De base quitar la navegación e implementar los listeners por seguridad, solo
+        // hasta después de seleccionar un inventario se configura.
+        configurarListenersCards(view);
+        deshabilitarNavegacion();
+
+        // Si no hay inventarios mostrarlo en un toast
+        if (!isAnyInventoryAvailable) {
+            showToast("There are no inventories");
+        }
+
         return view;
+    }
+
+    private void configurarSpinner(View view) {
+        // Obtener los inventarios de la BD
+        obtenerInventarios();
+
+        // Construir las opciones del spinner
+        List<String> opciones = isAnyInventoryAvailable ?
+                inventories.stream().map(Inventory::getName).toList():
+                List.of("No inventories detected");
+
+        // Esto hace que el spinner inicie sin "nada" seleccionado
+        if (isAnyInventoryAvailable) opciones.addFirst("");
+
+        // Después construir el adapter
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(
+                requireContext(),
+                android.R.layout.simple_spinner_item,
+                opciones
+        );
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerInventarios.setAdapter(spinnerAdapter);
+
+        // Configuración del listener
+        spinnerInventarios.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                manejarInventarioSeleccionado(adapterView, view, i, l);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                // Nada :p
+            }
+        });
+    }
+
+    private void manejarInventarioSeleccionado(AdapterView<?> parent, View view, int position, long id) {
+
+        if (selectedInventory != null && position == 0) {
+            selectedInventory = null;
+            deshabilitarNavegacion();
+            return;
+        }
+
+        if (position == 0) return;
+
+        selectedInventory = inventories.get(position);
+        habilitarNavegacion();
+    }
+
+    private void configurarListenersCards(View view) {
+        cardModifyProducts.setOnClickListener(v -> {
+            // Lógica de navegación hacia productos detailed
+
+            Bundle bundle = new Bundle();
+            bundle.putLong("id_inventory", selectedInventory.getId());
+            Navigation.findNavController(view)
+                    .navigate(R.id.action_manage_products_to_products_modify, bundle);
+        });
+
+        cardAuditProducts.setOnClickListener(v -> {
+            // Lógica de navegación hacia audit_products
+
+            Bundle bundle = new Bundle();
+            bundle.putLong("id_inventory", selectedInventory.getId());
+            Navigation.findNavController(view)
+                    .navigate(R.id.action_manage_products_to_audit_products, bundle);
+        });
+    }
+
+    private void obtenerInventarios() {
+        // SELECT de la base de datos
+        inventories = dbOps.getAllInventories();
+
+        // Si no hay inventarios, marcar esa flag
+        isAnyInventoryAvailable = !inventories.isEmpty();
+    }
+
+    private void habilitarNavegacion() {
+        cardModifyProducts.setEnabled(true);
+        cardModifyProducts.setCardBackgroundColor(ContextCompat.getColor(requireContext(), R.color.white));
+        cardAuditProducts.setEnabled(true);
+        cardAuditProducts.setCardBackgroundColor(ContextCompat.getColor(requireContext(), R.color.white));
+    }
+
+    private void deshabilitarNavegacion() {
+        cardModifyProducts.setEnabled(false);
+        cardModifyProducts.setCardBackgroundColor(ContextCompat.getColor(requireContext(), R.color.surface_disabled));
+        cardAuditProducts.setEnabled(false);
+        cardAuditProducts.setCardBackgroundColor(ContextCompat.getColor(requireContext(), R.color.surface_disabled));
+    }
+
+    private void showToast(String message){
+        Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
     }
 
 }
